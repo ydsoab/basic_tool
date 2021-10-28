43dbeebef3150cb119d9dfb530964af223fe12fa
ydsoab <93340088+ydsoab@users.noreply.github.com>
2021-10-28 11:41:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Basic-Tool")
+print("created by: ydsoab")
+print("Este script les ayudara a instalar algunas herramientas basicas para su terminal <3")
+print("[1] Herramientas")
+print("[0] Cerrar")
+
+tool = int(input("opcion: ") )
+if tool % 1 == 0:
+    print("[1]DDoS")
+    print("[2]OSINT")
+    tool_2 = int(input("Opcion:")
+    if tool_2 % 6 == 1
+        print("[1]DDoS-Ripper")
+        print("[2]Memcrashed")
+        print("[3]Hulk")
+        tool_3 = int(input("Opcion: ")
+        if tool_3 % 3 == 1:
+            os.system('git clone https://github.com/palahsu/DDoS-Ripper.git')
